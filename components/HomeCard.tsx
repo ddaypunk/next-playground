5cde283d2da32a3ae7cdcf313700d35df40ad12f
Andy Delso <andrew.delso@hyatt.com>
2021-10-27 14:09:39 -0500

    fix HomeCard to use only anchor

---
+++ components/HomeCard.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from "react";
+import styles from "../styles/Home.module.css";
+
+type Props = {
+    heading: string
+    description: string
+    page: string
+}
+
+export const HomeCard = ({ page, heading, description }: Props) =>
+    <a href={page} className={styles.card}>
+        <h2>{heading} &rarr;</h2>
+        <p>{description}</p>
+    </a>
